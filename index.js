e5cab49a60aba5f0bd7dcd3eb73aa0d7fd4fa869
amirmmolazemi <amirmohamad.molazemi@gmail.com>
2024-03-15 16:20:12 +0330

    fix: comment some lines

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import $ from "jquery";
-import bootstrap from 'bootstrap'
-import jscookie from 'jscookie'
-import Swal from "sweetalert2";
-import Chart from "chart.js";
-import { LW_Clock } from "lightweight-clock";
+// import $ from "jquery";
+// import bootstrap from 'bootstrap'
+// import jscookie from 'jscookie'
+// import Swal from "sweetalert2";
+// import Chart from "chart.js";
+// import { LW_Clock } from "lightweight-clock";
 
